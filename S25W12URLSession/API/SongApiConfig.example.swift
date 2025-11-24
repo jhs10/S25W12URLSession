b2f1da0e4064a475a5c1c6474081f93a82c0045e
jhs10 <jhs101019@naver.com>
2025-11-24 16:10:16 +0900

    🔐API Key

---
+++ S25W12URLSession/API/SongApiConfig.example.swift
@@ -0,0 +1,9 @@
+// SongAPIConfig.swift
+
+//struct SongApiConfig {
+struct SongApiConfigExample {
+    // Supabase의 connect 정보
+    static let projectURL = "https://zitawswoldixcndtjiej.supabase.co"
+    static let apiKey = yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppdGF3c3dvbGRpeGNuZHRqaWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NDY5ODIsImV4cCI6MjA3NzEyMjk4Mn0.O4HXubg4UcfEBglVa7FGrbUSa4yYh67RkmcLwD8-tpg"
+    static let serverURL = "\(projectURL)/rest/v1/songs?apikey=\(apiKey)"
+}
